42bd9d1b1186ba1537ba7cfd383887523978ad3f
zooneon <snsxhdz@naver.com>
2022-04-10 23:23:03 +0900

    테스트

---
+++ src/main/java/sju/capstone/docswant/security/HomeController.java
@@ -0,0 +1,13 @@
+package sju.capstone.docswant.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "docswant api server";
+    }
+}
