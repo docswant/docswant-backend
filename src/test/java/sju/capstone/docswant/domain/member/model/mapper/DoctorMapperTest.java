9acab74c251a06539e1d2e67cbc004c75e55a733
zooneon <snsxhdz@naver.com>
2022-04-23 02:07:27 +0900

    Test : 의사 mapper 테스트 작성

--- src/test/java/sju/capstone/docswant/domain/member/model/mapper/DoctorMapperTest.java
+++ src/test/java/sju/capstone/docswant/domain/member/model/mapper/DoctorMapperTest.java
@@ -1,4 +1,46 @@
-import static org.junit.jupiter.api.Assertions.*;
+package sju.capstone.docswant.domain.member.model.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sju.capstone.docswant.domain.member.model.dto.DoctorDto;
+import sju.capstone.docswant.domain.member.model.entity.doctor.Doctor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
 class DoctorMapperTest {
-  
+
+    @Autowired
+    private DoctorMapper mapper;
+
+    @Test
+    void 엔티티에서_DTO_테스트() {
+        //given
+        Doctor requestEntity = Doctor.builder().code("DOCTOR001").name("zooneon").major("orthopedics").build();
+
+        //when
+        DoctorDto.Response resultDto = mapper.toDto(requestEntity);
+
+        //then
+        assertThat(resultDto.getCode()).isEqualTo(requestEntity.getCode());
+        assertThat(resultDto.getName()).isEqualTo(requestEntity.getName());
+        assertThat(resultDto.getMajor()).isEqualTo(requestEntity.getMajor());
+    }
+
+    @Test
+    void DTO에서_엔티티_테스트() {
+        //given
+        DoctorDto.Request requestDto = DoctorDto.Request.builder().code("DOCTOR001").username("username").password("password").name("zooneon").major("orthopedics").build();
+
+        //when
+        Doctor resultEntity = mapper.toEntity(requestDto);
+
+        //then
+        assertThat(resultEntity.getCode()).isEqualTo(requestDto.getCode());
+        assertThat(resultEntity.getUsername()).isEqualTo(requestDto.getUsername());
+        assertThat(resultEntity.getPassword()).isEqualTo(requestDto.getPassword());
+        assertThat(resultEntity.getName()).isEqualTo(requestDto.getName());
+        assertThat(resultEntity.getMajor()).isEqualTo(requestDto.getMajor());
+    }
 }
