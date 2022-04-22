8dc07d6ad2894b39a244c12a322b84637a8e2640
zooneon <snsxhdz@naver.com>
2022-04-23 02:05:48 +0900

    Feat : 회원가입 시 인코딩된 password 저장할 수 있도록 추가

--- src/main/java/sju/capstone/docswant/domain/member/model/entity/Account.java
+++ src/main/java/sju/capstone/docswant/domain/member/model/entity/Account.java
@@ -23,7 +23,7 @@ public abstract class Account extends BaseTimeEntity implements Persistable<Stri
     @Column(name = "account_username", nullable = false, unique = true, length = 30)
     private String username;
 
-    @Column(name = "account_password", nullable = false, length = 50)
+    @Column(name = "account_password", nullable = false, length = 100)
     private String password;
 
     @Column(name = "account_type", updatable = false, insertable = false, length = 30)
@@ -40,6 +40,10 @@ public abstract class Account extends BaseTimeEntity implements Persistable<Stri
 
     public void setRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
+    }
+
+    public void setEncodedPassword(String encodedPassword) {
+        this.password = encodedPassword;
     }
 
     @Override
