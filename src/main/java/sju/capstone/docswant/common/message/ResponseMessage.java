56f2deb54f62da378718a6b20532782e1ff3a595
zooneon <snsxhdz@naver.com>
2022-04-23 02:00:55 +0900

    Feat : 응답 dto, 메시지 생성
    응답에 공통적으로 사용할 dto 생성, dto에는 응답 메시지, 응답 시간, 응답 데이터를 포함
    응답 메시지 수정해야 함

--- src/main/java/sju/capstone/docswant/common/message/ResponseMessage.java
+++ src/main/java/sju/capstone/docswant/common/message/ResponseMessage.java
@@ -1,2 +1,16 @@
-package sju.capstone.docswant.common.message;public class ResponseMessage {
+package sju.capstone.docswant.common.message;
+
+public class ResponseMessage {
+
+    //TODO: message 수정
+    //crud
+    public static final String CREATE_SUCCESS = "등록 성공";
+    public static final String READ_SUCCESS = "조회 성공";
+    public static final String UPDATE_SUCCESS = "수정 성공";
+    public static final String DELETE_SUCCESS = "삭제 성공";
+
+    public static final String CREATE_FAILED = "등록 실패";
+    public static final String READ_FAILED = "조회 실패";
+    public static final String UPDATE_FAILED = "수정 실패";
+    public static final String DELETE_FAILED = "삭제 실패";
 }
