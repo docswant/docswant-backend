dd6ae3e6568f8bf86258f218bfc4876f1c713145
zooneon <snsxhdz@naver.com>
2022-04-27 23:59:38 +0900

    Feat : 아이디 중복 검사 기능 추가

--- src/main/java/sju/capstone/docswant/domain/member/repository/account/AccountRepository.java
+++ src/main/java/sju/capstone/docswant/domain/member/repository/account/AccountRepository.java
@@ -1,4 +1,4 @@
-package sju.capstone.docswant.domain.member.repository;
+package sju.capstone.docswant.domain.member.repository.account;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sju.capstone.docswant.domain.member.model.entity.Account;
@@ -7,4 +7,5 @@ public interface AccountRepository extends JpaRepository<Account, String> {
 
     Account findByUsername(String username);
 
+    boolean existsByUsername(String username);
 }
--- src/main/java/sju/capstone/docswant/domain/member/service/account/AccountService.java
+++ src/main/java/sju/capstone/docswant/domain/member/service/account/AccountService.java
@@ -1,2 +1,11 @@
-package sju.capstone.docswant.domain.member.service.account;public interface AccountService {
+package sju.capstone.docswant.domain.member.service.account;
+
+import sju.capstone.docswant.domain.member.model.dto.AccountDto;
+
+public interface AccountService {
+
+    boolean isExistsUsername(String username);
+
+    String reissueAccessToken(AccountDto.Request requestDto);
+
 }
