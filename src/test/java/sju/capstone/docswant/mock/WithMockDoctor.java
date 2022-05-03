8386f9015a36068cbf37acd5f8503fa07394dd8e
zooneon <snsxhdz@naver.com>
2022-05-04 01:56:07 +0900

    Feat : 의사용 mock context 생성
    테스트에서 사용할 의사 인증 객체 생성
    SecurityContext에 저장하여 사용

--- src/test/java/sju/capstone/docswant/mock/WithMockDoctor.java
+++ src/test/java/sju/capstone/docswant/mock/WithMockDoctor.java
@@ -1,2 +1,11 @@
-package sju.capstone.docswant.mock;public interface WithMockDoctor {
+package sju.capstone.docswant.mock;
+
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockDoctorSecurityContextFactory.class)
+public @interface WithMockDoctor {
 }
