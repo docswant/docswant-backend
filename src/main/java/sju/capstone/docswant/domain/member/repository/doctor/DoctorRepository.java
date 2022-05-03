2b8b1b2ac939cc61d1719b1d6143faf31f751926
zooneon <snsxhdz@naver.com>
2022-05-03 22:27:35 +0900

    Feat : findByCode 추가
    가독성을 높이기 위해 findById 대신 사용

--- src/main/java/sju/capstone/docswant/domain/member/repository/doctor/DoctorRepository.java
+++ src/main/java/sju/capstone/docswant/domain/member/repository/doctor/DoctorRepository.java
@@ -3,6 +3,10 @@ package sju.capstone.docswant.domain.member.repository.doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sju.capstone.docswant.domain.member.model.entity.doctor.Doctor;
 
+import java.util.Optional;
+
 public interface DoctorRepository extends JpaRepository<Doctor, String> {
+
+    Optional<Doctor> findByCode(String code);
 
 }
--- src/main/java/sju/capstone/docswant/domain/member/repository/patient/PatientRepository.java
+++ src/main/java/sju/capstone/docswant/domain/member/repository/patient/PatientRepository.java
@@ -1,8 +1,13 @@
 package sju.capstone.docswant.domain.member.repository.patient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import sju.capstone.docswant.domain.member.model.entity.Account;
 import sju.capstone.docswant.domain.member.model.entity.patient.Patient;
 
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, String> {
+
+    Optional<Patient> findByCode(String code);
 
 }
