db812fecd9fb5304b6d763d1502c5b0d9c45fcba
zooneon <snsxhdz@naver.com>
2022-05-04 01:51:43 +0900

    Feat : 의사, 환자 연관관계 매핑
    의사, 환자 일대다 매핑
    연관관계 메서드 작성

--- src/test/java/sju/capstone/docswant/domain/member/model/entity/patient/PatientTest.java
+++ src/test/java/sju/capstone/docswant/domain/member/model/entity/patient/PatientTest.java
@@ -1,2 +1,23 @@
-package sju.capstone.docswant.domain.member.model.entity.patient;public class PatientTest {
+package sju.capstone.docswant.domain.member.model.entity.patient;
+
+import org.junit.jupiter.api.Test;
+import sju.capstone.docswant.domain.member.model.entity.doctor.Doctor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PatientTest {
+
+    @Test
+    void 의사_추가_테스트() {
+        //given
+        Patient patient = Patient.builder().code("PATIENT001").build();
+        Doctor doctor = Doctor.builder().code("DOCTOR001").build();
+
+        //when
+        patient.setDoctor(doctor);
+
+        //then
+        assertThat(patient.getDoctor()).isEqualTo(doctor);
+    }
+
 }
