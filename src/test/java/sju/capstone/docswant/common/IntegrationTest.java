82090a532667b2dea8966986985750de2b29e3ea
zooneon <snsxhdz@naver.com>
2022-05-10 21:33:51 +0900

    Fix : 통합 테스트 깨짐 해결
    sql script로 insert한 데이터들이 충돌하여 통합 테스트 시 테스트에 실패
    @Transactional 어노테이션을 추가하여 해결
    정확한 원인 파악 필요

--- src/test/java/sju/capstone/docswant/common/IntegrationTest.java
+++ src/test/java/sju/capstone/docswant/common/IntegrationTest.java
@@ -9,12 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import sju.capstone.docswant.security.authentication.token.JwtToken;
 
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @ExtendWith(RestDocumentationExtension.class)
+@Transactional
 @SpringBootTest
 public abstract class IntegrationTest {
 
