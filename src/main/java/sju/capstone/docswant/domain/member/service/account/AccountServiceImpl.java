3a15588b39fe1e9b166fb36bad7e21641839b837
zooneon <snsxhdz@naver.com>
2022-05-12 15:10:06 +0900

    Fix : 트랜잭션 어노테이션 추가

--- src/main/java/sju/capstone/docswant/domain/member/service/account/AccountServiceImpl.java
+++ src/main/java/sju/capstone/docswant/domain/member/service/account/AccountServiceImpl.java
@@ -3,6 +3,7 @@ package sju.capstone.docswant.domain.member.service.account;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sju.capstone.docswant.core.error.ErrorCode;
 import sju.capstone.docswant.core.error.exception.EntityNotFoundException;
 import sju.capstone.docswant.core.error.exception.InvalidValueException;
@@ -21,12 +22,14 @@ public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
     private final JwtToken jwtToken;
 
+    @Transactional(readOnly = true)
     @Override
     public boolean isExistsUsername(String username) {
         log.info("username duplication check. username = {}", username);
         return accountRepository.existsByUsername(username);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public String reissueAccessToken(String code, String refreshToken) {
         Account account = accountRepository.findByCode(code).orElseThrow(() -> new EntityNotFoundException(ErrorCode.ENTITY_NOT_FOUND));
