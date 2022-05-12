62609ec3d6c4017f1cc963430261f93ca0e61899
zooneon <snsxhdz@naver.com>
2022-05-12 14:29:50 +0900

    Feat : 데이터 추가

--- src/test/java/sju/capstone/docswant/common/factory/EntityFactory.java
+++ src/test/java/sju/capstone/docswant/common/factory/EntityFactory.java
@@ -5,6 +5,8 @@ import sju.capstone.docswant.domain.member.model.entity.patient.Patient;
 import sju.capstone.docswant.domain.member.model.entity.patient.PatientSchedule;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 public class EntityFactory {
 
@@ -34,6 +36,46 @@ public class EntityFactory {
                 .diseaseName("COVID-19")
                 .hospitalRoom(302)
                 .build();
+    }
+
+    public static List<Patient> getPatientEntities() {
+        PatientSchedule patientSchedule = PatientSchedule.builder()
+                .hospitalizationDate(LocalDate.of(2022, 5, 5))
+                .surgeryDate(LocalDate.of(2022, 5, 8))
+                .dischargeDate(LocalDate.of(2022, 5, 12))
+                .build();
+        return Arrays.asList(
+                Patient.builder()
+                        .code("PATIENT001")
+                        .username("PATIENT001")
+                        .password("password")
+                        .name("zooneon")
+                        .birthDate(LocalDate.of(1997, 8, 26))
+                        .patientSchedule(patientSchedule)
+                        .diseaseName("COVID-19")
+                        .hospitalRoom(300)
+                        .build(),
+                Patient.builder()
+                        .code("PATIENT002")
+                        .username("PATIENT002")
+                        .password("password")
+                        .name("zooneon")
+                        .birthDate(LocalDate.of(1997, 8, 26))
+                        .patientSchedule(patientSchedule)
+                        .diseaseName("COVID-19")
+                        .hospitalRoom(301)
+                        .build()
+                ,Patient.builder()
+                        .code("PATIENT003")
+                        .username("PATIENT003")
+                        .password("password")
+                        .name("zooneon")
+                        .birthDate(LocalDate.of(1997, 8, 26))
+                        .patientSchedule(patientSchedule)
+                        .diseaseName("COVID-19")
+                        .hospitalRoom(302)
+                        .build()
+        );
     }
 
 }
