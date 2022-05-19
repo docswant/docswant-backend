581a85b50ac7c74f9c518930e90532ad07617b1e
zooneon <snsxhdz@naver.com>
2022-05-20 02:20:16 +0900

    Feat : 리스트 dto mapper 추가

--- src/main/java/sju/capstone/docswant/domain/rounding/model/mapper/RoundingMapper.java
+++ src/main/java/sju/capstone/docswant/domain/rounding/model/mapper/RoundingMapper.java
@@ -4,24 +4,37 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import sju.capstone.docswant.domain.rounding.model.dto.RoundingDto;
 import sju.capstone.docswant.domain.rounding.model.entity.Rounding;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface RoundingMapper {
 
     RoundingMapper INSTANCE = Mappers.getMapper(RoundingMapper.class);
 
-    @Mappings({
-            @Mapping(target = "roundingSchedule.roundingDate", source = "roundingDate"),
-            @Mapping(target = "roundingSchedule.roundingTime", source = "roundingTime")
-    })
     Rounding toEntity(RoundingDto.Request requestDto);
 
     @Mappings({
             @Mapping(target = "roundingDate", source = "roundingSchedule.roundingDate"),
+            @Mapping(target = "patientName", source = "patient.name"),
             @Mapping(target = "roundingTime", source = "roundingSchedule.roundingTime")
     })
     RoundingDto.Response toDto(Rounding rounding);
+
+    default List<RoundingDto.ListResponse> toListDto(List<Rounding> roundings) {
+        List<RoundingDto.ListResponse> listResponseDto = new ArrayList<>();
+        MultiValueMap<Integer, RoundingDto.Response> roundingDtoMap = new LinkedMultiValueMap<>();
+        roundings.forEach(rounding -> roundingDtoMap.add(rounding.getPatient().getHospitalRoom(), toDto(rounding)));
+        for (Map.Entry<Integer, List<RoundingDto.Response>> entry : roundingDtoMap.entrySet()) {
+            listResponseDto.add(RoundingDto.ListResponse.builder().hospitalRoom(entry.getKey()).roundings(entry.getValue()).build());
+        }
+        return listResponseDto;
+    }
 
 }
