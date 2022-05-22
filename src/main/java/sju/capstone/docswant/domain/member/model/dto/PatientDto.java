0ebf3e3d979f7f55063e7fea5f6c70e27279de7d
zooneon <snsxhdz@naver.com>
2022-05-22 22:19:36 +0900

    Feat : 환자 회진 조회 dto, mapper 추가

--- src/main/java/sju/capstone/docswant/domain/member/model/dto/PatientDto.java
+++ src/main/java/sju/capstone/docswant/domain/member/model/dto/PatientDto.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,17 +19,10 @@ public class PatientDto {
 
     @Getter
     @NoArgsConstructor(access = AccessLevel.PROTECTED)
-    @JsonInclude(JsonInclude.Include.NON_NULL)
     public static class Request {
         @Size(max = 30)
         @NotBlank(message = "필수 값입니다.")
         private String code;
-
-        @Size(max = 30)
-        private String username;
-
-        @Size(max = 50)
-        private String password;
 
         @Size(max = 20)
         @NotBlank(message = "필수 값입니다.")
@@ -42,12 +36,6 @@ public class PatientDto {
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         private LocalDate hospitalizationDate;
 
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-        private LocalDate surgeryDate;
-
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
-        private LocalDate dischargeDate;
-
         @Size(max = 50)
         @NotBlank(message = "필수 값입니다.")
         private String diseaseName;
@@ -56,9 +44,84 @@ public class PatientDto {
         private int hospitalRoom;
 
         @Builder
-        public Request(String code, String username, String password, String name, LocalDate birthDate, LocalDate hospitalizationDate,
-                       LocalDate surgeryDate, LocalDate dischargeDate, String diseaseName, int hospitalRoom) {
+        public Request(String code, String name, LocalDate birthDate, LocalDate hospitalizationDate, String diseaseName, int hospitalRoom) {
             this.code = code;
+            this.name = name;
+            this.birthDate = birthDate;
+            this.hospitalizationDate = hospitalizationDate;
+            this.diseaseName = diseaseName;
+            this.hospitalRoom = hospitalRoom;
+        }
+    }
+
+    @Getter
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class Response {
+        private String code;
+        private String name;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate birthDate;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate hospitalizationDate;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate surgeryDate;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate dischargeDate;
+
+        private String diseaseName;
+        private int hospitalRoom;
+
+        @Builder
+        public Response(String code, String name, LocalDate birthDate, LocalDate hospitalizationDate,
+                        LocalDate surgeryDate, LocalDate dischargeDate, String diseaseName, int hospitalRoom) {
+            this.code = code;
+            this.name = name;
+            this.birthDate = birthDate;
+            this.hospitalizationDate = hospitalizationDate;
+            this.surgeryDate = surgeryDate;
+            this.dischargeDate = dischargeDate;
+            this.diseaseName = diseaseName;
+            this.hospitalRoom = hospitalRoom;
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class UpdateRequest {
+        @Size(max = 30)
+        private String username;
+
+        @Size(max = 50)
+        private String password;
+
+        @Size(max = 20)
+        private String name;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate birthDate;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate hospitalizationDate;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate surgeryDate;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+        private LocalDate dischargeDate;
+
+        @Size(max = 50)
+        private String diseaseName;
+
+        private int hospitalRoom;
+
+        @Builder
+        public UpdateRequest(String username, String password, String name, LocalDate birthDate, LocalDate hospitalizationDate,
+                             LocalDate surgeryDate, LocalDate dischargeDate, String diseaseName, int hospitalRoom) {
             this.username = username;
             this.password = password;
             this.name = name;
@@ -73,10 +136,9 @@ public class PatientDto {
 
     @Getter
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    public static class Response {
+    public static class PatientRoundingResponse {
         private String code;
-        private String username;
-        private String name;
+        private String patientName;
 
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         private LocalDate birthDate;
@@ -92,20 +154,27 @@ public class PatientDto {
 
         private String diseaseName;
         private int hospitalRoom;
+        private String doctorName;
+        private String doctorMajor;
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        private LocalTime roundingTime;
 
         @Builder
-        public Response(String code, String username, String name, LocalDate birthDate, LocalDate hospitalizationDate,
-                        LocalDate surgeryDate, LocalDate dischargeDate, String diseaseName, int hospitalRoom) {
+        public PatientRoundingResponse(String code, String patientName, LocalDate birthDate, LocalDate hospitalizationDate,
+                            LocalDate surgeryDate, LocalDate dischargeDate, String diseaseName, int hospitalRoom,
+                            String doctorName, String doctorMajor, LocalTime roundingTime) {
             this.code = code;
-            this.username = username;
-            this.name = name;
+            this.patientName = patientName;
             this.birthDate = birthDate;
             this.hospitalizationDate = hospitalizationDate;
             this.surgeryDate = surgeryDate;
             this.dischargeDate = dischargeDate;
             this.diseaseName = diseaseName;
             this.hospitalRoom = hospitalRoom;
+            this.doctorName = doctorName;
+            this.doctorMajor = doctorMajor;
+            this.roundingTime = roundingTime;
         }
     }
-
 }
