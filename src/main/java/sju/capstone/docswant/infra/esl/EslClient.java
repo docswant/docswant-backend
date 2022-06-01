7050640a36ccc47567fedc539f31fa977cdde1f6
zooneon <snsxhdz@naver.com>
2022-06-01 20:02:02 +0900

    Feat : esl client, dto 추가
    WebClient 이용
    테스트 후 dto 데이터 수정 필요

--- src/main/java/sju/capstone/docswant/infra/esl/EslClient.java
+++ src/main/java/sju/capstone/docswant/infra/esl/EslClient.java
@@ -1,14 +1,17 @@
 package sju.capstone.docswant.infra.esl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-import sju.capstone.docswant.domain.member.model.entity.patient.Patient;
-import sju.capstone.docswant.domain.rounding.model.entity.Rounding;
 
+@Slf4j
 @RequiredArgsConstructor
-public class EslService {
+@Component
+public class EslClient {
 
     private final WebClient webClient;
 
@@ -21,17 +24,27 @@ public class EslService {
     @Value("${esl.auth.user_password}")
     private String userPw;
 
-    public void sendChangeRequest(Patient patient, Rounding rounding) {
-        EslDto.ChangeContent changeContent = EslDto.ChangeContent.builder()
-                .tagId(patient.getId())
-                .patientName(patient.getName())
-                .doctorName(patient.getDoctor().getName())
-                .surgeryDday()
-                .dischargeDday()
-                .roundingTime()
-                .roundingOrder()
-                .build();
-
+    public Mono<Void> sendChangeRequest(EslDto.ChangeRequest requestDto) {
+        return sendAuthRequest().flatMap(authResponse -> {
+            String token = authResponse.getToken();
+            return webClient.post()
+                    .uri(editUri)
+                    .header("token", token)
+                    .body(Mono.just(requestDto), EslDto.ChangeRequest.class)
+                    .retrieve()
+                    .onStatus(httpStatus -> HttpStatus.BAD_REQUEST.equals(httpStatus),
+                            clientResponse -> {
+                                log.error("gateway data change request failed. status ={}", HttpStatus.BAD_REQUEST);
+                                return Mono.empty();
+                            })
+                    .onStatus(httpStatus -> HttpStatus.INTERNAL_SERVER_ERROR.equals(httpStatus),
+                            clientResponse -> {
+                                log.error("gateway data change request failed. status ={}", HttpStatus.INTERNAL_SERVER_ERROR);
+                                return Mono.empty();
+                            })
+                    .bodyToMono(Void.class)
+                    .doOnSuccess(response -> log.info("gateway data change request success."));
+        });
     }
 
     private Mono<EslDto.AuthResponse> sendAuthRequest() {
@@ -43,6 +56,18 @@ public class EslService {
                 .uri(loginUri)
                 .body(Mono.just(requestDto), EslDto.AuthRequest.class)
                 .retrieve()
-                .bodyToMono(EslDto.AuthResponse.class);
+                .onStatus(httpStatus -> HttpStatus.BAD_REQUEST.equals(httpStatus),
+                        clientResponse -> {
+                            log.error("gateway auth request failed. status ={}", HttpStatus.BAD_REQUEST);
+                            return Mono.empty();
+                        })
+                .onStatus(httpStatus -> HttpStatus.INTERNAL_SERVER_ERROR.equals(httpStatus),
+                        clientResponse -> {
+                            log.error("gateway auth request failed. status ={}", HttpStatus.INTERNAL_SERVER_ERROR);
+                            return Mono.empty();
+                        })
+                .bodyToMono(EslDto.AuthResponse.class)
+                .doOnSuccess(response -> log.info("gateway auth request success."));
     }
+
 }
