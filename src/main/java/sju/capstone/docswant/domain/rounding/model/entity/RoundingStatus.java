957b5b83fa360949882e0f2d42f4d072671399d1
zooneon <snsxhdz@naver.com>
2022-06-01 19:40:30 +0900

    Feat : 회진 상태 메시지 추가

--- src/main/java/sju/capstone/docswant/domain/rounding/model/entity/RoundingStatus.java
+++ src/main/java/sju/capstone/docswant/domain/rounding/model/entity/RoundingStatus.java
@@ -1,5 +1,12 @@
 package sju.capstone.docswant.domain.rounding.model.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum RoundingStatus {
-    TODO, DONE
+    TODO("예정"), DONE("완료");
+
+    private final String message;
 }
