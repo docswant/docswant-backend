6ba7a5cf1a1229fba72f7e82a59af74c29d2870b
zooneon <snsxhdz@naver.com>
2022-06-03 22:01:16 +0900

    Feat : 환자로 문의사항 전체 조회 추가

--- src/main/java/sju/capstone/docswant/domain/requirement/repository/RequirementRepository.java
+++ src/main/java/sju/capstone/docswant/domain/requirement/repository/RequirementRepository.java
@@ -3,12 +3,18 @@ package sju.capstone.docswant.domain.requirement.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import sju.capstone.docswant.domain.member.model.entity.patient.Patient;
 import sju.capstone.docswant.domain.requirement.model.entity.Requirement;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RequirementRepository extends JpaRepository<Requirement, Long> {
 
         Optional<Requirement> findById(Long id);
+
         Page<Requirement> findAllByPatientCode(String code, Pageable pageable);
+
+        List<Requirement> findAllByPatient(Patient patient);
+
 }
